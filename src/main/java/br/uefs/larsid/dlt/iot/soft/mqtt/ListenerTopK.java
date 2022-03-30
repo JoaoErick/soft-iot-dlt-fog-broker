0fd909c1ccdf7669015e1b4a4ae62b3a6e89240c
AllanCapistrano <allan.capistrano3@gmail.com>
2022-03-30 10:25:49 -0300

    Add ListenerTopK class.

---
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerTopK.java
@@ -0,0 +1,71 @@
+package br.uefs.larsid.dlt.iot.soft.mqtt;
+
+import br.uefs.larsid.dlt.iot.soft.services.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class ListenerTopK implements IMqttMessageListener {
+
+  private boolean debugModeValue;
+  private Controller impl;
+  private MQTTClient MQTTClientUp;
+  private MQTTClient MQTTClientHost;
+
+  /**
+   *
+   * @param impl
+   * @param clienteMQTT_Up
+   * @param clienteMQTT_Host
+   * @param topico
+   * @param qos
+   */
+  public ListenerTopK(
+    Controller impl,
+    MQTTClient MQTTClientUp,
+    MQTTClient MQTTClientHost,
+    String topic,
+    int qos
+  ) {
+    this.MQTTClientUp = MQTTClientUp;
+    this.MQTTClientHost = MQTTClientHost;
+    this.MQTTClientUp.subscribe(qos, this, topic);
+    this.impl = impl;
+  }
+
+  @Override
+  public void messageArrived(String topic, MqttMessage message)
+    throws Exception {
+    final String[] params = topic.split("/");
+
+    /** [0] -> TOP_K_HEALTH; [1] -> Id-Request; [2] -> K */
+    final int k = Integer.valueOf(params[2]);
+
+    printlnDebug("==== Fog UP gateway -> Fog gateway  ====");
+
+    printlnDebug("Requisição recebida: " + topic);
+
+    if (params[0].equals("TOP_K_HEALTH")) {
+      byte[] messageEmpty = "".getBytes();
+
+      MQTTClientHost.publish(topic, messageEmpty, 1);
+
+      List<String> scoreListEmpty = new ArrayList<String>();
+
+      impl.getTopKScores().put(params[1], scoreListEmpty);
+
+      // Inicia o cálculo de top k
+      impl.calculateTopK(params[1], k);
+    }
+  }
+
+  private void printlnDebug(String str) {
+    if (debugModeValue) {
+      System.out.println(str);
+    }
+  }
+    
+}
