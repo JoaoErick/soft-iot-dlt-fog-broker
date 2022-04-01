514a2dc9eae333924522b07546bbd40882d9f93a
JoaoErick <jerick1415@outlook.com>
2022-04-01 11:06:34 -0300

    Revert "Remove convertWithStream method and change topic."
    This reverts commit 09f054304bf6f23147de0541b13b7f5ef08ac598.

--- src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/Listener.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/Listener.java
@@ -1,46 +1,33 @@
 package br.uefs.larsid.dlt.iot.soft.mqtt;
 
 import br.uefs.larsid.dlt.iot.soft.services.Controller;
+
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class Listener implements IMqttMessageListener {
 
-  /*-------------------------Constantes---------------------------------------*/
   private static final String TOP_K_RES = "TOP_K_HEALTH_RES";
-  /*--------------------------------------------------------------------------*/
 
   private boolean debugModeValue;
   private Controller controllerImpl;
   private MQTTClient MQTTClientHost;
 
-  /**
-   * Método Construtor
-   *
-   * @param controllerImpl Controller - Controller que fará uso desse Listener.
-   * @param MQTTClientHost MQTTClient - Cliente MQTT do gateway inferior.
-   * @param topic String - Tópico que será ouvido
-   * @param qos int - Qualidade de serviço do tópico que será ouvido.
-   * @param debugModeValue boolean - Modo para debugar o código.
-   */
   public Listener(
-    Controller controllerImpl,
-    MQTTClient MQTTClientHost,
-    String topic,
-    int qos,
-    boolean debugModeValue
-  ) {
+      Controller controllerImpl,
+      MQTTClient MQTTClientHost,
+      String topic,
+      int qos) {
     this.controllerImpl = controllerImpl;
     this.MQTTClientHost = MQTTClientHost;
-    this.debugModeValue = debugModeValue;
 
     this.MQTTClientHost.subscribe(qos, this, topic);
   }
 
-  /**
-   * 
-   */
   @Override
   public void messageArrived(String topic, MqttMessage message)
     throws Exception {
@@ -50,41 +37,36 @@ public class Listener implements IMqttMessageListener {
       printlnDebug("==== Bottom gateway -> Fog gateway  ====");
 
       String messageContent = new String(message.getPayload());
-      Map<String, Integer> bottomMap = controllerImpl.convertStrigToMap(messageContent);
+      Map<String, Integer> bottomMap = this.convertWithStream(messageContent);
       Map<String, Integer> fogMap = this.controllerImpl.getMapById(params[1]);
-
+      
       fogMap.putAll(bottomMap);
-      controllerImpl.putScores(params[1], fogMap);
+      controllerImpl.putScores(params[1], fogMap); 
 
       printlnDebug(
-        "Top-K response received: " +
+        "Top-k response received: " +
         controllerImpl.getMapById(params[1]).toString()
       );
     }
   }
 
-  /**
-   * 
-   * @param str
-   */
+  private Map<String, Integer> convertWithStream(String mapAsString) {
+    Map<String, Integer> map = Arrays.stream(mapAsString.split(","))
+      .map(entry -> entry.split("="))
+      .collect(Collectors.toMap(entry -> entry[0], entry -> Integer.parseInt(entry[1])));
+    return map;
+  }
+
   private void printlnDebug(String str) {
     if (isDebugModeValue()) {
       System.out.println(str);
     }
   }
 
-  /**
-   * 
-   * @return
-   */
   public boolean isDebugModeValue() {
     return debugModeValue;
   }
 
-  /**
-   * 
-   * @param debugModeValue
-   */
   public void setDebugModeValue(boolean debugModeValue) {
     this.debugModeValue = debugModeValue;
   }
--- src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerTopK.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerTopK.java
@@ -1,90 +1,71 @@
 package br.uefs.larsid.dlt.iot.soft.mqtt;
 
 import br.uefs.larsid.dlt.iot.soft.services.Controller;
+
 import java.util.HashMap;
 import java.util.Map;
+
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class ListenerTopK implements IMqttMessageListener {
 
-  /*-------------------------Constantes---------------------------------------*/
-  private static final String TOP_K_FOG = "TOP_K_HEALTH_FOG";
-  private static final String TOP_K = "TOP_K_HEALTH";
-  private static final int QOS = 1;
-  /*--------------------------------------------------------------------------*/
-
   private boolean debugModeValue;
+  private Controller impl;
   private MQTTClient MQTTClientUp;
   private MQTTClient MQTTClientHost;
-  private Controller controllerImpl;
 
   /**
    *
-   * @param controllerImpl Controller - Controller que fará uso desse Listener.
-   * @param MQTTClientUp MQTTClient - Cliente MQTT do gateway superior.
-   * @param MQTTClientHost MQTTClient - Cliente MQTT do gateway inferior.
-   * @param topic String - Tópico que será ouvido
-   * @param qos int - Qualidade de serviço do tópico que será ouvido.
-   * @param debugModeValue boolean - Modo para debugar o código.
+   * @param impl
+   * @param clienteMQTT_Up
+   * @param clienteMQTT_Host
+   * @param topico
+   * @param qos
    */
   public ListenerTopK(
-    Controller controllerImpl,
+    Controller impl,
     MQTTClient MQTTClientUp,
     MQTTClient MQTTClientHost,
     String topic,
-    int qos,
-    boolean debugModeValue
+    int qos
   ) {
     this.MQTTClientUp = MQTTClientUp;
     this.MQTTClientHost = MQTTClientHost;
-    this.controllerImpl = controllerImpl;
-    this.debugModeValue = debugModeValue;
     this.MQTTClientUp.subscribe(qos, this, topic);
+    this.impl = impl;
   }
 
-  /**
-   * 
-   */
   @Override
   public void messageArrived(String topic, MqttMessage message)
     throws Exception {
     final String[] params = topic.split("/");
 
+    /** [0] -> TOP_K_HEALTH; [1] -> Id-Request; [2] -> K */
     final int k = Integer.valueOf(params[2]);
 
     printlnDebug("==== Fog UP gateway -> Fog gateway  ====");
 
-    printlnDebug("Request received: " + topic);
+    printlnDebug("Requisição recebida: " + topic);
 
-    if (params[0].equals(TOP_K_FOG)) {
+    if (params[0].equals("TOP_K_HEALTH")) {
       byte[] messageEmpty = "".getBytes();
 
-      String topicDown = String.format(
-        "%s/%s/%s",
-        TOP_K,
-        params[1],
-        params[2]
-      );
-
-      MQTTClientHost.publish(topicDown, messageEmpty, QOS);
+      MQTTClientHost.publish(topic, messageEmpty, 1);
 
       Map<String, Integer> scoreMapEmpty = new HashMap<String, Integer>();
 
-      controllerImpl.getTopKScores().put(params[1], scoreMapEmpty);
+      impl.getTopKScores().put(params[1], scoreMapEmpty);
 
-      // Inicia o cálculo de Top-K
-      controllerImpl.calculateTopK(params[1], k);
+      // Inicia o cálculo de top k
+      impl.calculateTopK(params[1], k);
     }
   }
 
-  /**
-   * 
-   * @param str
-   */
   private void printlnDebug(String str) {
     if (debugModeValue) {
       System.out.println(str);
     }
   }
+    
 }
