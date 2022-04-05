4cb324d04769640234f32b0db6739315c5a39fba
AllanCapistrano <allan.capistrano3@gmail.com>
2022-04-05 08:01:11 -0300

    Add Top-0 verification.

--- src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerTopK.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerTopK.java
@@ -51,25 +51,37 @@ public class ListenerTopK implements IMqttMessageListener {
     throws Exception {
     final String[] params = topic.split("/");
 
-    final int k = Integer.valueOf(params[2]); //TODO: Tratar aqui requisição k = 0
+    final int k = Integer.valueOf(params[2]);
 
     printlnDebug("==== Fog UP gateway -> Fog gateway  ====");
 
     printlnDebug("Request received: " + topic);
 
-    if (params[0].equals(TOP_K_FOG)) {
-      byte[] messageEmpty = "".getBytes();
+    if (k == 0) {
+      printlnDebug("Top-K = 0");
 
-      String topicDown = String.format("%s/%s/%s", TOP_K, params[1], params[2]);
+      this.controllerImpl.sendEmptyTopK(params[1]);
+    } else {
 
-      MQTTClientHost.publish(topicDown, messageEmpty, QOS);
+      if (params[0].equals(TOP_K_FOG)) {
+        byte[] messageEmpty = "".getBytes();
 
-      Map<String, Integer> scoreMapEmpty = new HashMap<String, Integer>();
+        String topicDown = String.format(
+          "%s/%s/%s",
+          TOP_K,
+          params[1],
+          params[2]
+        );
 
-      controllerImpl.getTopKScores().put(params[1], scoreMapEmpty);
+        MQTTClientHost.publish(topicDown, messageEmpty, QOS);
 
-      // Inicia o cálculo de Top-K
-      controllerImpl.calculateTopK(params[1], k);
+        Map<String, Integer> scoreMapEmpty = new HashMap<String, Integer>();
+
+        controllerImpl.getTopKScores().put(params[1], scoreMapEmpty);
+
+        // Inicia o cálculo de Top-K
+        controllerImpl.calculateTopK(params[1], k);
+      }
     }
   }
 
