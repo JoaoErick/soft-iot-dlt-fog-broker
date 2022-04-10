6a9d1c0c11a57106de8ce6c332cc4c23d2d5f263
AllanCapistrano <allan.capistrano3@gmail.com>
2022-04-09 22:18:04 -0300

    Create loadConnectedDevices and updateValuesSensors.

--- src/main/java/br/uefs/larsid/dlt/iot/soft/model/ControllerImpl.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/model/ControllerImpl.java
@@ -1,23 +1,33 @@
 package br.uefs.larsid.dlt.iot.soft.model;
 
+import br.uefs.larsid.dlt.iot.soft.entity.Device;
+import br.uefs.larsid.dlt.iot.soft.entity.Sensor;
 import br.uefs.larsid.dlt.iot.soft.mqtt.Listener;
 import br.uefs.larsid.dlt.iot.soft.mqtt.ListenerTopK;
 import br.uefs.larsid.dlt.iot.soft.mqtt.MQTTClient;
 import br.uefs.larsid.dlt.iot.soft.services.Controller;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ControllerImpl implements Controller {
 
   /*-------------------------Constantes---------------------------------------*/
   private static final int QOS = 1;
   private static final String TOP_K = "TOP_K_HEALTH_FOG/#";
+  private static final String TOP_K_RES_FOG = "TOP_K_HEALTH_FOG_RES/";
   private static final String TOP_K_RES = "TOP_K_HEALTH_RES/#";
   private static final String INVALID_TOP_K = "INVALID_TOP_K/#";
-  private static final String TOP_K_RES_FOG = "TOP_K_HEALTH_FOG_RES/";
   private static final String INVALID_TOP_K_FOG = "INVALID_TOP_K_FOG/";
   /*--------------------------------------------------------------------------*/
 
@@ -25,7 +35,9 @@ public class ControllerImpl implements Controller {
   private MQTTClient MQTTClientHost;
   private MQTTClient MQTTClientUp;
   private String childs;
-  public Map<String, Map<String, Integer>> topKScores = new HashMap<String, Map<String, Integer>>();
+  private String urlAPI;
+  private Map<String, Map<String, Integer>> topKScores = new HashMap<String, Map<String, Integer>>();
+  private List<Device> devices;
 
   public ControllerImpl() {}
 
@@ -35,6 +47,8 @@ public class ControllerImpl implements Controller {
   public void start() {
     this.MQTTClientHost.connect();
     this.MQTTClientUp.connect();
+
+    this.loadConnectedDevices(ClientIotService.getApiIot(urlAPI));
 
     new Listener(this, MQTTClientHost, INVALID_TOP_K, QOS, debugModeValue);
     new Listener(this, MQTTClientHost, TOP_K_RES, QOS, debugModeValue);
@@ -55,6 +69,56 @@ public class ControllerImpl implements Controller {
     this.MQTTClientHost.disconnect();
     this.MQTTClientUp.disconnect();
     // TODO Desinscrever dos tópicos.
+  }
+
+  public void updateValuesSensors() {
+    for (Device d : this.devices) {
+      d.getLastValueSensors();
+    }
+  }
+
+  private void loadConnectedDevices(String strDevices) {
+    List<Device> devicesTemp = new ArrayList<Device>();
+
+    try {
+      printlnDebug("JSON load:");
+      printlnDebug(strDevices);
+
+      JSONArray jsonArrayDevices = new JSONArray(strDevices);
+
+      for (int i = 0; i < jsonArrayDevices.length(); i++) {
+        JSONObject jsonDevice = jsonArrayDevices.getJSONObject(i);
+        ObjectMapper mapper = new ObjectMapper();
+        Device device = mapper.readValue(jsonDevice.toString(), Device.class);
+
+        devicesTemp.add(device);
+
+        List<Sensor> tempSensors = new ArrayList<Sensor>();
+        JSONArray jsonArraySensors = jsonDevice.getJSONArray("sensors");
+
+        for (int j = 0; j < jsonArraySensors.length(); j++) {
+          JSONObject jsonSensor = jsonArraySensors.getJSONObject(j);
+          Sensor sensor = mapper.readValue(jsonSensor.toString(), Sensor.class);
+          tempSensors.add(sensor);
+        }
+
+        device.setSensors(tempSensors);
+      }
+    } catch (JsonParseException e) {
+      System.out.println(
+        "Verify the correct format of 'DevicesConnected' property in configuration file."
+      );
+    } catch (JsonMappingException e) {
+      System.out.println(
+        "Verify the correct format of 'DevicesConnected' property in configuration file."
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    this.devices = devicesTemp;
+
+    printlnDebug("Amount of devices connected: " + this.devices.size());
   }
 
   /**
@@ -127,7 +191,6 @@ public class ControllerImpl implements Controller {
     printlnDebug(message);
 
     MQTTClientUp.publish(INVALID_TOP_K_FOG + topicId, message.getBytes(), QOS);
-    
   }
 
   /**
@@ -188,5 +251,21 @@ public class ControllerImpl implements Controller {
       .getBytes();
 
     this.MQTTClientUp.publish(TOP_K_RES_FOG + topicId, payload, QOS);
+  }
+
+  public String getUrlAPI() {
+    return urlAPI;
+  }
+
+  public void setUrlAPI(String urlAPI) {
+    this.urlAPI = urlAPI;
+  }
+
+  public List<Device> getDevices() {
+    return devices;
+  }
+
+  public void setDevices(List<Device> devices) {
+    this.devices = devices;
   }
 }
