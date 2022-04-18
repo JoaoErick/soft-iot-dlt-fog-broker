da237a7872784c0248bda9b54823cdd75dbd0036
AllanCapistrano <allan.capistrano3@gmail.com>
2022-04-18 12:30:24 -0300

    Fix JsonProperty.

--- src/main/java/br/uefs/larsid/dlt/iot/soft/entity/Sensor.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/entity/Sensor.java
@@ -1,20 +1,23 @@
 package br.uefs.larsid.dlt.iot.soft.entity;
 
 import br.uefs.larsid.dlt.iot.soft.model.ClientIotService;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.json.JSONObject;
 
 public class Sensor {
 
   private String id;
   private String type;
-  private int collectionTime;
-  private int publishingTime;
   private int value;
   private String urlAPI;
 
-  public Sensor(String urlAPI) {
-    this.urlAPI = urlAPI;
-  }
+  @JsonProperty("collection_time")
+  private int collectionTime;
+
+  @JsonProperty("publishing_time")
+  private int publishingTime;
+
+  public Sensor() {}
 
   public void getValue(String idDevice) {
     String url = String.format("%s/%s/%s", urlAPI, idDevice, this.id);
@@ -66,5 +69,13 @@ public class Sensor {
 
   public void setValue(int value) {
     this.value = value;
+  }
+
+  public String getUrlAPI() {
+    return urlAPI;
+  }
+
+  public void setUrlAPI(String urlAPI) {
+    this.urlAPI = urlAPI;
   }
 }
