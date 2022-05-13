26a63ec294b685e6bd98ff9a784fc0725b6db5e6
JoaoErick <jerick1415@outlook.com>
2022-05-13 10:45:04 -0300

    Calling start in the constructor.

--- src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/MQTTClient.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/MQTTClient.java
@@ -14,8 +14,10 @@ import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 
 public class MQTTClient implements MqttCallbackExtended, MQTTClientService {
 
+  /*-------------------------Constantes---------------------------------------*/
   private static final int QOS = 1;
   private static final String CONNECTED = "CONNECTED/1/1";
+  /*--------------------------------------------------------------------------*/
 
   private String ip;
   private String port;
@@ -40,13 +42,16 @@ public class MQTTClient implements MqttCallbackExtended, MQTTClientService {
     this.port = port;
     this.userName = userName;
     this.password = password;
+
+    this.start();
   }
 
   /**
-   * Inicializa o bundle.
+   * Inicializa o client MQTT.
    */
   public void start() {
-    printlnDebug("Starting SOFT-IoT-Fog-Broker bundle...");
+    printlnDebug("Starting MQTTClient...");
+    
     this.serverURI = String.format("tcp://%s:%s", this.ip, this.port);
 
     this.mqttOptions = new MqttConnectOptions();
@@ -61,10 +66,10 @@ public class MQTTClient implements MqttCallbackExtended, MQTTClientService {
   }
 
   /**
-   * Finaliza o bundle.
+   * Finaliza o client MQTT.
    */
   public void stop() {
-    printlnDebug("Finishing SOFT-IoT-Fog-Broker bundle...");
+    printlnDebug("Finishing MQTTClient....");
   }
 
   /**
