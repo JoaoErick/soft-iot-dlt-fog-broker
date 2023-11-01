297fab126d77be188e072181b8a6178c43dad236
JoaoErick <jerick1415@outlook.com>
2023-10-31 21:01:30 -0300

    feat: add mqtt listener to receive the real score of each device

---
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerDeviceScore.java
@@ -0,0 +1,90 @@
+package br.uefs.larsid.dlt.iot.soft.mqtt;
+
+import br.uefs.larsid.dlt.iot.soft.services.Controller;
+
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author João Erick Barbosa
+ */
+public class ListenerDeviceScore implements IMqttMessageListener {
+
+  /*------------------------------ Constants ------------------------------*/ 
+  private static Logger log = Logger.getLogger(ListenerDeviceScore.class.getName());
+  /*-------------------------------------------------------------------------*/
+
+  private boolean debugModeValue;
+  private Controller controllerImpl;
+  private MQTTClient MQTTClientHost;
+
+  /**
+   * Constructor Method.
+   *
+   * @param controllerImpl Controller - Controller that will make use of this Listener.
+   * @param MQTTClientHost MQTTClient - Bottom Gateway MQTT Client.
+   * @param topics         String[] - Topics that will be subscribed.
+   * @param qos            int - Quality of service of the topic that will be heard.
+   * @param debugModeValue boolean - Mode to debug the code.
+   */
+  public ListenerDeviceScore(
+      Controller controllerImpl,
+      MQTTClient MQTTClientHost,
+      String[] topics,
+      int qos,
+      boolean debugModeValue) {
+    this.controllerImpl = controllerImpl;
+    this.MQTTClientHost = MQTTClientHost;
+    this.debugModeValue = debugModeValue;
+
+    for (String topic : topics) {
+      this.MQTTClientHost.subscribe(qos, this, topic);
+    }
+  }
+
+  @Override
+  public void messageArrived(String topic, MqttMessage message)
+      throws Exception {
+    String msg = new String(message.getPayload());
+    String deviceId;
+    int score;
+
+    JsonObject jsonProperties = new Gson().fromJson(msg, JsonObject.class);
+
+    /* Extract data */
+    deviceId = jsonProperties.get("HEADER").getAsJsonObject().get("NAME").getAsString();
+    score = jsonProperties.get("BODY").getAsJsonObject().get("score").getAsInt();
+
+    printlnDebug("------------- Score Device Received -------------");
+    printlnDebug("Device ID: " + deviceId);
+    printlnDebug("Score: " + score);
+    printlnDebug("----------------------------------------------\n");
+
+    this.controllerImpl.addDeviceScore(deviceId, score);
+
+    if (this.controllerImpl.getDevicesScores().size() >= this.controllerImpl.getDeviceIdsAuths().size()) {
+      this.controllerImpl.calculateTopKDown();
+    }
+        
+  }
+
+  private void printlnDebug(String str) {
+    if (isDebugModeValue()) {
+      log.info(str);
+    }
+  }
+
+  public boolean isDebugModeValue() {
+    return debugModeValue;
+  }
+
+  public void setDebugModeValue(boolean debugModeValue) {
+    this.debugModeValue = debugModeValue;
+  }
+}
